1113fa5c31fda850f4d5c7d2aaa37ab2ae1ba734
sahil3748 <isahil3748@gmail.com>
2025-07-20 14:14:13 +0530

    Update service worker and bootstrap versioning
    - Updated the service worker version in flutter_bootstrap.js to "4151473593" for improved caching.
    - Changed the hash for flutter_bootstrap.js in flutter_service_worker.js to reflect the new version for accurate resource management.

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "160166075"
+    serviceWorkerVersion: "4151473593"
   }
 });
--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "df8558208ad6af3bf3310782b9f438c0
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "062be43239681968167d4a932e7d230f",
+"flutter_bootstrap.js": "beb6050e9b23821a0d91a9ecfcc765cf",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
